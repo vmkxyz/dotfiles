d1dda252a164c1b8626dccd116e0593c9f8c3042
vmk <>
2024-03-24 11:40:54 +0100

    add
    i actually added it in previous commit but i suck at using git so...

--- .local/bin/doas_on_arch.sh
+++ .local/bin/doas_on_arch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# this script symlinks doas to sudo so programs using sudo directly work, makes doas kinda like sudo and FORCE REMOVES SUDO, it ussually warks for however some stuff in the system might break by doing that, you've been warned
+# this script symlinks doas to sudo so programs using sudo directly work, makes doas kinda like sudo and FORCE REMOVES SUDO! it ussually warks for however some stuff in the system might break by doing that, you've been warned
 # see https://wiki.archlinux.org/title/Doas
 # run this script as root
 
