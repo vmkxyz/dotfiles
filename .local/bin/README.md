13f19b07d388d059950ed05c1a0c2db095a7f003
vmk <>
2024-03-24 11:38:38 +0100

    .local/bin/README.md
    update readme

--- .local/bin/README.md
+++ .local/bin/README.md
@@ -1,3 +1,4 @@
 some of these scripts might be very old, not working or changing system level configurations
 
 DO NOT RUN these scripts without carefully reading them and understanding what they do first
+actualy NEVER RUN sripts frop others as root without knowing what they do
---
+++ .local/bin/doas_on_arch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# this script symlinks doas to sudo so programs using sudo directly work, makes doas kinda like sudo and FORCE REMOVES SUDO, it ussually warks for however some stuff in the system might break by doing that, you've been warned
+# see https://wiki.archlinux.org/title/Doas
+# run this script as root
+
+pacman -S --needed opendoas
+pacman -rdd sudo # Scary!
+ln -s $(which doas) /usr/bin/sudo
+ln -s $(which doas) /bin/sudo
+ln -s $(which doas) /sbin/sudo
+if [ ! -e "/etc/doas.conf" ]; then
+    touch "/etc/doas.conf"
+fi
+echo 'permit :wheel as root
+permit persist :wheel' >> /etc/doas.conf
