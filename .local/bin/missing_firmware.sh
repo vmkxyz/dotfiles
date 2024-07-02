52efe761c1a8b0c43b456c50d75f121bf3e259d4
vmkxyz <vmkxyz@outlook.com>
2024-07-02 13:42:53 +0200

    add terminus font install

--- .local/bin/missing_firmware.sh
+++ .local/bin/missing_firmware.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # RUN AS ROOT
 
-pacman -Syu --needed linux-firmware-qlogic linux-headers
+pacman -Syu --needed linux-firmware-qlogic linux-headers terminus-font
 paru -Syu --needed aic94xx-firmware wd719x-firmware upd72020x-fw ast-firmware
