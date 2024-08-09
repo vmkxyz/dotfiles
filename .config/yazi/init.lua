405f6e25e70d1d86374206e68e494b0be9fa2f74
vmkxyz <vmkxyz@outlook.com>
2024-08-09 14:51:13 +0200

    move to yazi v0.3.0

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,67 +1,36 @@
 ---@diagnostic disable: undefined-global
 
 -- Draw a full bar around window
-function Manager:render(area)
-	self.area = area
+function Linemode:custom()
+  local year = os.date("%Y")
+  local time = (self._file.cha.modified or 0) // 1
 
-	local chunks = ui.Layout()
-		:direction(ui.Layout.HORIZONTAL)
-		:constraints({
-			ui.Constraint.Ratio(MANAGER.ratio.parent, MANAGER.ratio.all),
-			ui.Constraint.Ratio(MANAGER.ratio.current, MANAGER.ratio.all),
-			ui.Constraint.Ratio(MANAGER.ratio.preview, MANAGER.ratio.all),
-		})
-		:split(area)
-
-  local bar = function(c, x, y)
-  	x, y = math.max(0, x), math.max(0, y)
-  	return ui.Bar(ui.Rect { x = x, y = y, w = ya.clamp(0, area.w - x, 1), h = math.min(1, area.h) }, ui.Bar.TOP)
-  		:symbol(c)
+  if time > 0 and os.date("%Y", time) == year then
+    time = os.date("%b %d %H:%M", time)
+  else
+    time = time and os.date("%b %d  %Y", time) or ""
   end
 
-	return ya.flat {
-		-- Borders
-    ui.Border(area, ui.Border.ALL):type(ui.Border.ROUNDED),
- 		ui.Bar(chunks[1], ui.Bar.RIGHT),
- 		ui.Bar(chunks[3], ui.Bar.LEFT),
-
- 		bar("┬", chunks[1].right - 1, chunks[1].y),
- 		bar("┴", chunks[1].right - 1, chunks[1].bottom - 1),
- 		bar("┬", chunks[2].right, chunks[2].y),
- 		bar("┴", chunks[2].right, chunks[1].bottom - 1),
-
-    -- Parent
- 		Parent:render(chunks[1]:padding(ui.Padding.xy(1))),
- 		-- Current
- 		Current:render(chunks[2]:padding(ui.Padding.y(1))),
- 		-- Preview
- 		Preview:render(chunks[3]:padding(ui.Padding.xy(1))),
-	}
+  local size = self._file:size()
+  return ui.Line(string.format(" %s %s ", size and ya.readable_size(size):gsub(" ", "") or "-", time))
 end
+
+
 
 -- Show user and hostname in top bar
-function Header:host()
-	if ya.target_family() ~= "unix" then
-		return ui.Line {}
-	end
-	return ui.Line { ui.Span(ya.user_name() .. "@" .. ya.host_name()):fg("lightgreen"):bold(true), ui.Span(":") }
-end
+Status:children_add(function()
+  local h = cx.active.current.hovered
+  if not h or not ya.user_name then
+    return ui.Line {}
+  end
 
-function Header:render(area)
-	self.area = area
-
-	local right = ui.Line { self:count(), self:tabs() }
-	local left = ui.Line {
-    self:host(),
-    self:cwd(math.max(0, area.w - right:width())):fg("blue"),
-    ui.Span("/"):fg("blue"):bold(true),
-    ui.Span(tostring(cx.active.current.hovered.name)):fg("white"):bold(true),
+  return ui.Line {
+    ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+    ui.Span(":"),
+    ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+    ui.Span(" "),
   }
-	return {
-		ui.Paragraph(area, { left }),
-		ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
-	}
-end
+end, 500, Status.RIGHT)
 
 -- Show symlink path in status bar
 function Status:name()
@@ -90,3 +59,5 @@ function Status:render(area)
 	}
 end
 
+
+require("full-border"):setup()
