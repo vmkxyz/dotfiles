0adb9d1eedeffa0ae407bf0951166ef16801b318
vmkxyz <vmkxyz@outlook.com>
2024-09-15 10:42:55 +0200

    telescope ignore more dirs/files

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,10 +2,9 @@ local builtin = require('telescope.builtin')
 
 require('telescope').setup {
   defaults = {
-    file_ignore_patterns = {"Games/*", "Pictures/*", "Music/*", "dotfiles/*", "**/*.pdf", "**/*.jpg", "**/*.jpeg", "**/*.pptx", "**/*.odp"},
+    file_ignore_patterns = {"Games/*", "Pictures/*", "Music/*", "dotfiles/*", "**/*.pdf", "**/*.jpg", "**/*.jpeg", "**/*.pptx", "**/*.odp", "**/*.png", "**/*.dng", "**/*.mp4", "**/*.mp3", "**/*.mkv", "/target", "/git", "/projects", "Android/*"},
   },
 }
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
-
