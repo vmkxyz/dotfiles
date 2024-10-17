bc7a77656efc10a276ff3bee17d068dcd1470555
vmkxyz <vmkxyz@outlook.com>
2024-10-17 17:31:04 +0200

    qtile config - work in progress

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+wl-paste --type text --watch cliphist store &
+wl-paste --type image --watch cliphist store &
+syncthing serve --no-browser &
+kdeconnect-indicator &
+rnd_wall_shell_wbg &
+hypridle &
+pidof lowbat || lowbat &
+dunst &
