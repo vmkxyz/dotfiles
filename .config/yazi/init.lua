dc1d3547ce33085906bb70e07f7aa748bafdeafb
vmkxyz <vmkxyz@outlook.com>
2024-10-31 18:47:50 +0100

    yazi: update plugins and cleanup

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,6 +1,7 @@
----@diagnostic disable: undefined-global
+require("full-border"):setup()
+require("no-status"):setup()
 
--- Draw a full bar around window
+-- Inline file info
 function Linemode:custom()
   local year = os.date("%Y")
   local time = (self._file.cha.modified or 0) // 1
@@ -14,50 +15,3 @@ function Linemode:custom()
   local size = self._file:size()
   return ui.Line(string.format(" %s %s ", size and ya.readable_size(size):gsub(" ", "") or "-", time))
 end
-
-
-
--- Show user and hostname in top bar
-Status:children_add(function()
-  local h = cx.active.current.hovered
-  if not h or not ya.user_name then
-    return ui.Line {}
-  end
-
-  return ui.Line {
-    ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
-    ui.Span(":"),
-    ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
-    ui.Span(" "),
-  }
-end, 500, Status.RIGHT)
-
--- Show symlink path in status bar
-function Status:name()
-	local h = cx.active.current.hovered
-	if not h then
-		return ui.Span("")
-	end
-
- 	local linked = ""
- 	if h.link_to ~= nil then
- 		linked = " -> " .. tostring(h.link_to)
- 	end
- 	return ui.Span(" " .. h.name .. linked)
-end
-
--- Remove file size from status bar
-function Status:render(area)
-	self.area = area
-
-	local left = ui.Line { self:mode(), self:name() }
-	local right = ui.Line { self:permissions(), self:position() }
-	return {
-		ui.Paragraph(area, { left }),
-		ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
-		table.unpack(Progress:render(area, right:width())),
-	}
-end
-
-
-require("full-border"):setup()
