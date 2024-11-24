9b34899e88f1b7468a3bb8a6c714b39c9f4bcf10
vmkxyz <vmkxyz@outlook.com>
2024-11-24 14:08:57 +0100

    add luke smith's <++> bind

--- .config/nvim/lua/vmk/remap.lua
+++ .config/nvim/lua/vmk/remap.lua
@@ -15,3 +15,5 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader><leader>", "/<++><CR>ca<", { noremap = true, silent = true })
