140b5fdab9ebeb83f90f548cc509c558dda8943a
vmk <vmkz@cock.li>
2025-07-24 10:19:11 +0200

    update yazi configs

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,6 +1,11 @@
 require("full-border"):setup()
 require("no-status"):setup()
 
+-- Sync yank clipboard across instances
+require("session"):setup {
+	sync_yanked = true,
+}
+
 -- Inline file info
 function Linemode:custom()
   local year = os.date("%Y")
@@ -15,3 +20,11 @@ function Linemode:custom()
   local size = self._file:size()
   return ui.Line(string.format(" %s %s ", size and ya.readable_size(size):gsub(" ", "") or "-", time))
 end
+
+-- Show username and hostname in header
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ""
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
